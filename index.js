b17e49353602be7a679647c038d517fa48a609f0
junaid <junaidali1507@gmail.com>
2021-07-17 14:54:50 +0530

    added files

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const mongoose = require("mongoose");
 const app = express();
 
 const authRoute = require('./routes/auth');
+const sgRoute = require('./routes/sendgrid');
+const twilio = require('./routes/twilio');
 
 
 const dbURI = "mongodb://localhost/authentication";
 app.use(express.json());
 app.use(express.urlencoded());
 app.use('/api/auth' , authRoute);
-// app.use('/api/verify' , sgRoute);
+app.use('/api/verify' , sgRoute);
+app.use('/api/phone-verify' ,twilio);
 
 
-
+ 
 mongoose.connect(dbURI , {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
@@ -21,4 +24,4 @@ db.on("error", (err)=> {console.error(err)});
 
 db.once("open", ()=>{console.log("DB started successfully")});
 
-app.listen(8080, ()=>{console.log("Server started : 8080")});
+app.listen(8090, ()=>{console.log("Server started : 8090")});
