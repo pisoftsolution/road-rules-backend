fd1f24ff0cbf126b39121c3ca40b98bcf99f2392
junaid <junaidali1507@gmail.com>
2021-08-20 13:43:45 +0530

    completed eslint and prettier

--- index.js
+++ index.js
@@ -3,18 +3,25 @@ const mongoose = require("mongoose");
 const app = express();
 var cors = require('cors')
 
-const authRoute = require('./routes/auth'); 
-const twilio = require('./routes/twilio');
+const authRoute = require('./routes/auth');
+// const twilio = require('./routes/twilio');
 const sgRoute = require('./routes/sendgrid');
+const adminADD = require('./routes/admin');
+const instRoute = require('./routes/instructor');
+const slotRoute = require('./routes/slot');
 
-const dbURI = "mongodb+srv://root:junaid@cluster0.qxafi.mongodb.net/test"; 
+const dbURI = "mongodb://localhost/authentication"; 
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
-app.use('/api/auth' , authRoute);
-app.use('/api/verify' ,twilio);
-app.use('/api/verify' , sgRoute);
+app.use('/api/auth',authRoute);
+// app.use('/api/phone-verify' ,twilio);
+app.use('/api/verify',sgRoute);
+app.use('/api/instructor',instRoute);
+app.use('/api/admin',adminADD);
+
+app.use('/api/slot',slotRoute);
  
 mongoose.connect(dbURI , {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -24,4 +31,4 @@ db.on("error", (err)=> {console.error(err)});
 
 db.once("open", ()=>{console.log("DB Started Successfully")});
 
-app.listen(8090, ()=>{console.log("Server Started : 8090")});
+app.listen(8095, ()=>{console.log("Server Started : 8095")});
