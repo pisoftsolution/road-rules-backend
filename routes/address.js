bece7e7709cdcbdf6eb5060ddd32f0d4d255794e
junaid <junaidali1507@gmail.com>
2021-08-24 19:45:33 +0530

    changes done

--- routes/address.js
+++ routes/address.js
@@ -6,4 +6,4 @@ router.post('/add-address',address.addAddress);
 // router.get('/get-address-by-user' ,address.getAddressByUser);
 router.get('/get-user-by-id' ,address.getUserById); 
 
-module.exports = router;
+module.exports = router; 
