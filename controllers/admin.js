fff9132948e3ecd82843ecd77f17a52be11d9a6f
jshaik49 <jshaik49@gmail.com>
2021-08-26 17:29:02 +0530

    Added findOne and check-in instructor in modify slot

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,23 +2,23 @@ const User = require('../models/Slot');
 
 exports.getUsers = (req, res) => {
   User.find({})
-    .then(users => {
-    return res.status(200).json({ users: users })
+    .then((users) => {
+      return res.status(200).json({ users: users });
     })
-    .catch(err => {
-    return res.status(400).json({ msg: err.message })
-    })
-}
+    .catch((err) => {
+      return res.status(400).json({ msg: err.message });
+    });
+};
 
 exports.getUserById = (req, res) => {
-    if (!req.query.id) {
-    return res.status(400).json({ msg: "You Need To Send ID!" })
-    }
-    User.find({ _id: req.query.id })
-    .then(user => {
-    return res.status(200).json({ user: user });
+  if (!req.query.id) {
+    return res.status(400).json({ msg: 'You Need To Send ID!' });
+  }
+  User.findOne({ _id: req.query.id })
+    .then((user) => {
+      return res.status(200).json({ user: user });
     })
-    .catch(err => {
-        return res.status(200).json({ msg: err.message });
-    })
-}
+    .catch((err) => {
+      return res.status(200).json({ msg: err.message });
+    });
+};
