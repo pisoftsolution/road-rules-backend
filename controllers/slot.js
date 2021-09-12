f098d19217156cdeff24d008147f471479581ed5
junaid <junaidali1507@gmail.com>
2021-09-12 12:34:51 +0530

    added files

--- controllers/slot.js
+++ controllers/slot.js
@@ -1,73 +1,65 @@
 const Slot = require('../models/SlotSchema');
-const Instructor = require('../models/InstructorSchema');
 
-exports.addSlot = async (req, res) => {
-  if (
-    !req.body.date ||
+exports.addSlot = (req, res) => {
+  if (!req.body.date ||
     !req.body.time ||
     !req.body.clientLimit ||
     !req.body.instructor ||
     !req.body.instructorName ||
-    !req.body.status
-  ) {
-    return res.status(400).json({ msg: 'Invalid Data' });
+    !req.body.status ||
+    !req.body.booking) {
+    res.status(400).json({ msg: "This is invalid data" });
   }
-  const instructors = await Instructor.findById(req.body.instructor);
-  if (instructors) {
-    let slot = new Slot({
-      ...req.body
-    });
-    slot
-      .save()
-      .then((b) => {
-        if (b) {
-          res.status(200).json({ b });
-        }
-      })
-      .catch((err) => {
-        res.status(400).json({ err });
-      });
-  } else {
-    res.status(400).json({ msg: 'Instructor Does Not Exist' });
-  }
-};
-
-exports.modifySlot = async (req, res) => {
-  const instructors = await Instructor.findById(req.body.instructor);
-  if (instructors) {
-    let b = new Slot({
-      ...req.body
-    });
-    Slot.findById(req.query.id)
-      .then((b) => {
-        if (b) {
-          (b.date = req.body.date),
-            (b.time = req.body.time),
-            (b.clientLimit = req.body.b.clientLimit),
-            (b.instructor = req.body.instructor),
-            (b.instructorName = req.body.instructorName),
-            (b.status = req.body.status),
-            (b.booking = req.body.booking);
-          b.save().then((b2) => {
-            res.status(200).json(b2);
-          });
-        }
-      })
-      .catch((err) => {
-        res.status(400).json({ err });
-      });
-  } else {
-    res.status(400).json({ msg: 'Instructor Does Not Exist' });
-  }
+  let about = new Slot({
+    date: req.body.date,
+    time: req.body.time,
+    clientLimit: req.body.clientLimit,
+    instructor: req.body.instructor,
+    instructorName: req.body.instructorName,
+    status: req.body.status,
+    booking: req.body.booking, 
+  });
+  about.save()
+    .then(b => {
+      if (b) {
+        res.status(200).json({ b });
+      }
+    })
+    .catch(err => {
+      res.status(400).json({ err });
+    })
 };
 
 exports.getSlots = (req, res) => {
+  console.log(req.query);
   Slot.find({})
-    .then((b) => {
-      return res.status(200).json({ b: b });
+    .then(b => {
+      res.status(200).json({ b: b });
     })
-    .catch((err) => {
-      return res.status(400).json({ msg: err.message });
+    .catch(err => {
+      res.status(400).json({ msg: err.message });
+    });
+};
+
+exports.modifySlot = (req, res) => {
+  Slot.findById(req.query.id)
+    .then(b => {
+      if (b) {
+        b.date = req.body.date;
+        b.time = req.body.time;
+        b.clientLimit = req.body.clientLimit;
+        b.instructor = req.body.instructor;
+        b.instructorName = req.body.instructorName;
+        b.status = req.body.status;
+        b.booking = req.body.booking;
+        b.save()
+          .then(b2 => {
+            res.status(200).json(b2);
+          });
+      }
+    })
+    .catch(err => {
+      res.status(400).json({ err });
     });
 };
 
@@ -83,6 +75,7 @@ exports.getSlotById = (req, res) => {
       return res.status(200).json({ msg: err.message });
     });
 };
+
 exports.searchSlotByDate = (req, res) => {
   if (!req.query.date) {
     return res.status(400).json({ msg: 'Please Enter Date!' });
--- routes/instructor.js
+++ routes/instructor.js
@@ -4,6 +4,7 @@ const instructor = require('../controllers/instructor');
 router.post('/add-instructor',instructor.addInstructor);
 router.get('/get-instructors',instructor.getAll);
 router.get('/instructor',instructor.getInstructorById);
+router.put('/update-instructor', instructor.updateInstructor);
 router.delete('/delete-instructor',instructor.deleteInstructor);
 
 module.exports = router;
