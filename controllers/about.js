ac689d340c57d5e6c4049901535050f0dbd53d56
jshaik49 <jshaik49@gmail.com>
2021-10-03 06:15:49 +0530

    contact us post request -sendgrid error

--- controllers/about.js
+++ controllers/about.js
@@ -1,46 +1,46 @@
 const AboutSchema = require('../models/AboutSchema');
 
 exports.addAbout = (req, res) => {
-  if ( !req.body.text) {
-    res.status(400).json({ msg: "This is invalid data" });
+  if (!req.body.text) {
+    res.status(400).json({ msg: 'This is invalid data' });
   }
   let about = new AboutSchema({
     text: req.body.text
   });
-  about.save()
-    .then(b => {
+  about
+    .save()
+    .then((b) => {
       if (b) {
         res.status(200).json({ b });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json({ err });
-    })
+    });
 };
 
 exports.getAbout = (req, res) => {
   console.log(req.query);
   AboutSchema.find({})
-    .then(b => {
+    .then((b) => {
       res.status(200).json({ b: b });
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json({ msg: err.message });
     });
 };
 
-exports.updateAbout = (req, res) => { 
+exports.updateAbout = (req, res) => {
   AboutSchema.findById(req.query.id)
-    .then(b => {
+    .then((b) => {
       if (b) {
         b.text = req.body.text;
-        b.save()
-          .then(b2 => {
-            res.status(200).json(b2);
-          });
+        b.save().then((b2) => {
+          res.status(200).json(b2);
+        });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json({ err });
     });
 };
